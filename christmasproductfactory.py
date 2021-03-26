d832f9f1036ee3a8966db9859efcf7eb6ac7c49b
Xaitin <xaitin120@gmail.com>
2021-03-25 21:27:28 -0700

    Adding exceptions to christmasproductfactory.py

--- christmasproductfactory.py
+++ christmasproductfactory.py
@@ -1,3 +1,4 @@
+from InvalidDataError import InvalidDataError
 from productfactory import ProductFactory
 from workshop import Workshop
 from reindeer import Reindeer
@@ -15,6 +16,10 @@ class ChristmasProductFactory(ProductFactory):
         has_lactose = other_info['has_lactose']
         has_nuts = other_info['has_nuts']
         color = other_info['colour']
+        if has_lactose != "N":
+            raise InvalidDataError("Candy Canes are Lactose Free")
+        elif has_nuts != "N":
+            raise InvalidDataError("Candy Canes are Nut Free")
         return Canes(has_nuts, has_lactose, name, description, product_id, color)
 
     def create_toy(self, item):
@@ -30,6 +35,8 @@ class ChristmasProductFactory(ProductFactory):
         width = formatted[1]
         min_age = other_info['min_age']
         has_batteries = other_info['has_batteries']
+        if has_batteries != "N":
+            raise InvalidDataError("Santa's Workshop doesnt need batteries")
         return Workshop(has_batteries, min_age, name, description, product_id, height, width, rooms)
 
     def create_stuffed_animal(self, item):
@@ -42,4 +49,10 @@ class ChristmasProductFactory(ProductFactory):
         size = other_info['size']
         stuffing = other_info['stuffing']
         glow = other_info['has_glow']
+        if fabric != "Cotton":
+            raise InvalidDataError("Reindeer Fabric must be Cotton")
+        elif stuffing != "Wool":
+            raise InvalidDataError("Reindeer Stuffing must be Wool")
+        elif glow != "Y":
+            raise InvalidDataError("Reindeer have Glow in the Dark Noses")
         return Reindeer(stuffing, size, fabric, name, description, product_id, glow)
