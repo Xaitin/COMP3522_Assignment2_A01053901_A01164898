1403e51c346429f4e73e9136e021fac30b2b6f5f
Nhilayne <Nhilayne@gmail.com>
2021-03-25 21:58:07 -0700

    read_order_file bugfix

--- orderprocessor.py
+++ orderprocessor.py
@@ -15,8 +15,6 @@ class OrderProcessor:
     def read_order_file(self, file_name):
         orders = pd.read_excel(file_name)
         # print('Excel Sheet to Dict:', orders.to_dict(orient='records'))
-        details = orders.drop(['holiday'], ['order_number'], ['product_id'], ['item'], ['name'])
-        details = details.to_dict(orient='records')
         orders = orders.to_dict(orient='records')
         for order in orders:
             if order['holiday'] == 'Christmas':
@@ -26,6 +24,13 @@ class OrderProcessor:
             else:
                 factory = SpookyProductFactory
 
+            # details = orders.drop(['holiday'], ['order_number'], ['product_id'], ['item'], ['name'])
+            details = order.copy()
+            del details['holiday']
+            del details['order_number']
+            del details['product_id']
+            del details['item']
+            del details['name']
             new_order = Order(order['order_number'], order['product_id'], order['quantity'], order['item'],
                               order['name'], details, factory)
             self._order_list.append(new_order)
