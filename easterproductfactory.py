77f10ddc05114db665adb9ccaf0fcb85bc158e57
Xaitin <xaitin120@gmail.com>
2021-03-25 21:19:02 -0700

    Adding exceptions to easterproductfactory.py

--- easterproductfactory.py
+++ easterproductfactory.py
@@ -1,5 +1,6 @@
+from InvalidDataError import InvalidDataError
 from productfactory import ProductFactory
-from toffee import Toffee
+from eggs import Eggs
 from eBunny import EBunny
 from rbunny import RBunny
 
@@ -14,10 +15,12 @@ class EasterProductFactory(ProductFactory):
         description = other_info['description']
         has_lactose = other_info['has_lactose']
         has_nuts = other_info['has_nuts']
-        variety = other_info['variety']
-        if variety != "Sea Salt" or variety != "Regular":
-            variety = "Sea Salt"
-        return Toffee(has_nuts, has_lactose, name, description, product_id, variety)
+        pack_size = other_info['pack_size']
+        if has_lactose != "N":
+            raise InvalidDataError("Creme Eggs must be Lactose Free")
+        elif has_nuts != "Y":
+            raise InvalidDataError("Creme Eggs may have nuts")
+        return Eggs(has_nuts, has_lactose, name, description, product_id, pack_size)
 
     def create_toy(self, item):
         # creating Robot Bunnies for easter
@@ -29,6 +32,10 @@ class EasterProductFactory(ProductFactory):
         num_effects = other_info['num_sound']
         min_age = other_info['min_age']
         has_batteries = other_info['has_batteries']
+        if has_batteries != "Y":
+            raise InvalidDataError("Robot Bunnies need Batteries")
+        if color != "Orange" or color != "Blue" or color != "Pink":
+            raise InvalidDataError("Robot Bunnies must be Orange Blue or Pink")
         return RBunny(has_batteries, min_age, name, description, product_id, num_effects, color)
 
     def create_stuffed_animal(self, item):
@@ -41,4 +48,8 @@ class EasterProductFactory(ProductFactory):
         size = other_info['size']
         stuffing = other_info['stuffing']
         color = other_info['colour']
+        if fabric != "Linen":
+            raise InvalidDataError("Easter Bunnies must be made out of Linen")
+        if stuffing != "Polyester Fiberfill":
+            raise InvalidDataError("Easter Bunnies must be stuffed with Polyester Fiberfill")
         return EBunny(stuffing, size, fabric, name, description, product_id, color)
